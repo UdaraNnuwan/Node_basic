42135d5d146fc23241e39498b32b058cc924d62d
UdaraNnuwan <84951777+UdaraNnuwan@users.noreply.github.com>
2025-08-19 12:41:41 +0530

    initial Commit Node Basid

---
+++ dataType.js
@@ -0,0 +1,15 @@
+var a=1
+let b =3
+const c =4
+
+var a=5//can use
+// var b =5 Cannot use 
+// let b =5 cannot use
+
+b=8
+a=10
+// c=12 //Assignment to constant variable
+
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ ifElse.js
@@ -0,0 +1,14 @@
+let a=1
+
+if(a==="1"){
+    console.log("hey")
+}else{
+    console.log("ho")
+}
+
+
+if(a=="1"){
+    console.log("hey")
+}else{
+    console.log("ho")
+}
