ce9975b4b8a0487e9b719260f9387e5e2b6e05e1
UdaraNnuwan <84951777+UdaraNnuwan@users.noreply.github.com>
2025-08-20 12:49:24 +0530

    Create Monogo DB

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -6,23 +6,33 @@
 // .listen(3000)
 
 
-const express=require("express")
-const bodyParser=require('body-parser')
+const express = require("express")
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+const dotEnv=require('dotenv')
 
-const app=express()
+const app = express()
+dotEnv.config()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Server Up and Running ")
 })
 
-const route=require("./routes/route")
-app.use("/route",route)
+const route = require("./routes/route")
+mongoose.connect("mongodb://localhost:27017/test").then(() => {
+    console.log("Connected to database")
+})
+    .catch((error) => {
+        console.log(error)
+    })
+app.use("/route", route)
+// app.use("/posts",route)
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server Up and Runnign")
 })
--- nodeServer/routes/route.js
+++ nodeServer/routes/route.js
@@ -1,5 +1,5 @@
 const router=require("express").Router()
-
+const post=require("../model/post")
 router.get("/ping",(req,res)=>{
     // res.send("pong")
     res.json({
@@ -12,4 +12,41 @@ router.post("/ping",(req,res)=>{
     res.json(request)
 })
 
+router.post("/save",async(req,res)=>{
+    const title=req.body.title
+    const description=req.body.description
+
+    const newPost=new post(
+        {
+            title:title,
+            description:description
+        }
+    )
+    const savepost =await newPost.save()
+    res.json(savepost)
+})
+
+router.get("/save",async(req,res)=>{
+    const getDetails=await post.find({})
+    res.json(getDetails)
+})
+
+router.get("/save/:id",async(req,res)=>{
+    const getDetails=await post.findOne({_id:req.params.id})
+    res.json(getDetails)
+})
+
+router.patch("/save/:id",async(req,res)=>{
+    const _id=req.params.id
+     const update=await post.findByIdAndUpdate(_id,{$set:{
+        description:req.body.description
+    }})
+    res.json(update)
+})
+
+router.delete("/save/:id",async(req,res)=>{
+    const _id=req.params.id
+     await post.findByIdAndDelete({_id:_id})
+    res.json({message:"Removed"})
+})
 module.exports=router
